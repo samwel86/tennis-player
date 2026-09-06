f39d43d21483695d2a8ed574d0419b5aa1782c8a
samwel86 <64667739+samwel86@users.noreply.github.com>
2026-09-06 19:20:49 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAyCl8BgoLd7lYSFO4XCkeOGEhwMpGCtAo",
+    authDomain: "tennis-stats-b2b0e.firebaseapp.com",
+    databaseURL: "https://tennis-stats-b2b0e-default-rtdb.firebaseio.com",
+    projectId: "tennis-stats-b2b0e",
+    storageBucket: "tennis-stats-b2b0e.firebasestorage.app",
+    messagingSenderId: "821472668066",
+    appId: "1:821472668066:web:e93d6bec4af93fde5a6afb",
+    measurementId: "G-VJC0J5X45C"
+};
